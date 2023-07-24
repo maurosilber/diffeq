0ede93e9498afd8b689b5541f3db00d129427919
Mauro Silberberg <maurosilber@gmail.com>
2023-07-24 09:45:54 -0300

    Add example test for numpy

---
+++ src/diffeq/numpy/tests/test_example.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from .. import AllSteps, Euler, ODEProblem, solve
+
+
+def test_solve():
+    def rhs(t, y, p, dy):
+        dy[:] = -y
+
+    problem = ODEProblem(
+        rhs,
+        0.0,
+        np.arange(5.0),
+        np.zeros(0),
+    )
+    solution = solve(
+        problem,
+        t_end=1,
+        solver=Euler(dt=0.01),
+        saver=AllSteps(),
+    )
+
+    assert solution.t[0] == problem.t
+    assert solution.t[1] == 0.01
+    assert np.isclose(solution.t[-1], 1)
+    assert len(solution.t) == len(solution.y)
+    assert solution.y.shape[1] == problem.y.size
